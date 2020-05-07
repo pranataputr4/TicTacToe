44d115cf97a78f21ddafe814947227d0d9ced9d8
Raymond Kurniawan <r.kurniawan@ti.ukdw.ac.id>
2020-05-07 21:08:19 +0700

    Buat layout game tic tac toe sederhana
    Entah kenapa ga bisa di tempatku untuk dicoba, tolong test dulu sbelum di merge / yang lain...

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.tictactoe;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,4 +22,10 @@ public class MainActivity extends AppCompatActivity {
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
+
+    public void GoToGamePage(View view){
+        Intent intent = new Intent(this, GamePage.class);
+        startActivity(intent);
+    }
+
 }
