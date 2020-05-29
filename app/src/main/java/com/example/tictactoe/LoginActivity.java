2c5eece2946ee48af268501f2395587d041fb5a9
Sinung <sinung.purnama@ti.ukdw.ac.id>
2020-05-29 08:17:33 +0700

    Login, Main, Register, Setting, User Profile, Xml, Autentikasi, Realtime Database, Reset Password Firebase Email

--- app/src/main/java/com/example/tictactoe/LoginActivity.java
+++ app/src/main/java/com/example/tictactoe/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.tictactoe;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -11,16 +12,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     Button mLoginBtn;
-    TextView mCreateUserBtn;
+    TextView mCreateUserBtn, mLupaPassword;
     EditText  mEmail, mPassword;
     FirebaseAuth mFirebase;
 
@@ -34,6 +38,7 @@ public class LoginActivity extends AppCompatActivity {
         mFirebase = FirebaseAuth.getInstance();
         mLoginBtn = findViewById(R.id.loginbtn);
         mCreateUserBtn = findViewById(R.id.daftarUser);
+        mLupaPassword = findViewById(R.id.lupaPassword);
 
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,6 +78,43 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), Register.class));
+            }
+        });
+
+        mLupaPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText resetMail = new EditText(v.getContext());
+                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password?");
+                passwordResetDialog.setMessage("Silahkan Masuk ke Email anda untuk mendapatkan Reset Password");
+                passwordResetDialog.setView(resetMail);
+
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String mail = resetMail.getText().toString();
+                        mFirebase.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(LoginActivity.this,"Reset Link Sent To Your Email",Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(LoginActivity.this, "Error! Reset Link Not Sent" + e.getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                passwordResetDialog.create().show();
             }
         });
     }
